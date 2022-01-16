778f191603906d7735a30715fa07678b7524480e
Lexian <71324166+lLexian@users.noreply.github.com>
2022-01-16 10:25:11 +0000

    Update ecm2bin.sh
    Alternative prompts and swapped out ecm for emc-tools command.

--- ecm2bin.sh
+++ ecm2bin.sh
@@ -4,9 +4,9 @@
 
 main_process () {
 
-	input_1=$(zenity --file-selection --icon-name=applications-games --title="Pick Game To Convert" --file-filter="*.ecm" 2> /dev/null)
+	input_1=$(zenity --file-selection --icon-name=applications-games --title="Select The *.bin.ecm File You Wish To Convert" --file-filter="*.ecm" 2> /dev/null)
 
-	echo ""ecm-uncompress "'$input_1'" "'$input_1.bin'""" | xclip;
+	echo ""ecm2bin "'$input_1'" "'$input_1.bin'""" | xclip;
 
 	xclip -o | $SHELL;
 	
@@ -16,9 +16,9 @@ main_process () {
 
 process_complete () {
 
-	zenity --notification --window-icon=face-smile-big.png --text="Done! Enjoy Your Rom!"  2> /dev/null
+	zenity --notification --window-icon=face-smile-big.png --text="Complete!"  2> /dev/null
 
-	zenity --question --icon-name=applications-games --text="Convert Another File(s)?" --timeout=10 --default-cancel --cancel-label=No --ok-label="Bring it on" 2> /dev/null
+	zenity --question --icon-name=applications-games --text="Convert Another File(s)?" --timeout=10 --default-cancel --cancel-label=No --ok-label="Yes" 2>/dev/null
 	if [ $? = 0 ] ; then
 		
 		main_process
@@ -31,42 +31,8 @@ process_complete () {
 }
 
 
-
-
-
-
-
-
-
 ##################### Start Sequence
 
 main_process
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 chmod +x ecm2bin.sh
